2c614ba53aa61d05c8e885b091b241df33cd3ab2
Sahid Hasan <shrsahid4@gmail.com>
2026-07-19 16:06:56 +0600

    Initial

---
+++ Select and Clause.sql
@@ -0,0 +1,34 @@
+USE hr_training;
+
+-- SELECT * FROM attendance; 
+SELECT * FROM departments; 
+-- SELECT * FROM employee_projects; 
+-- SELECT * FROM employees LIMIT 5,10; 
+-- SELECT * FROM jobs; 
+-- SELECT * FROM locations; 
+-- SELECT * FROM performance_reviews; 
+-- SELECT * FROM projects; 
+-- SELECT * FROM salary_history; 
+
+
+-- SELECT first_name,Hire_date,curdate() AS today,
+-- timestampdiff(year,Hire_date,curdate()) AS Age
+-- FROM employees 
+-- WHERE department_id=1 
+-- AND Hire_date >= '2020-01-01'
+-- ORDER BY Age DESC; 
+
+SELECT first_name,DATE_FORMAT(Hire_date,'%d-%b-%y') Hire_Date,salary, round(salary/1000)*1000
+FROM employees 
+WHERE department_id=1
+AND Hire_date >= '2020-01-01';
+
+SELECT * 
+FROM employees
+-- WHERE (Department_id= 1 OR Department_id=3)
+WHERE Department_id IN (1,3)
+AND salary>=100000 
+AND employment_status='Active'
+ORDER BY Salary ;
+
+
