3918e73c05ce0d727f3b39b0ee74b0a482728e31
Sahid Hasan <shrsahid4@gmail.com>
2026-08-27 01:29:07 +0600

    Modifying Q1 solution and Adding Q2 question Solution

---
+++ Projects/LS-Q2.sql
@@ -0,0 +1,52 @@
+USE hr_training;
+
+-- Finding Employee who work at least 3 year in a company
+SELECT employee_id,timestampdiff(year,Hire_date,curdate()) AS Age
+FROM employees
+WHERE timestampdiff(year,Hire_date,curdate()) >=3;
+
+SELECT *
+FROM employees;
+
+SELECT *
+FROM jobs;
+
+-- Checking Purpose
+SELECT job_id,employee_id,salary
+FROM employees
+WHERE employee_id=34;
+
+-- Finding avg as per job title
+
+SELECT job_id,ROUND(AVG(salary),2) AS t_avg
+FROM employees
+GROUP BY job_id;
+
+
+
+-- Final Result 
+SELECT employee_id,t_avg,salary,em.job_id,
+(t_avg-salary) as salary_gap,
+timestampdiff(year,Hire_date,curdate()) AS Age
+FROM employees AS em
+JOIN(SELECT job_id,ROUND(AVG(salary),2) AS t_avg
+FROM employees
+GROUP BY job_id) AS av
+ON em.job_id = av.job_id
+WHERE t_avg*(1 - 0.1)>salary AND timestampdiff(year,Hire_date,curdate()) >=3
+ORDER BY salary_gap DESC;
+
+
+-- Counting Underpaying employees
+SELECT COUNT(employee_id)
+FROM (SELECT employee_id,t_avg,salary,em.job_id,
+(t_avg-salary) as salary_gap,
+timestampdiff(year,Hire_date,curdate()) AS Age
+FROM employees AS em
+JOIN(SELECT job_id,ROUND(AVG(salary),2) AS t_avg
+FROM employees
+GROUP BY job_id) AS av
+ON em.job_id = av.job_id
+WHERE t_avg*(1 - 0.1)>salary AND timestampdiff(year,Hire_date,curdate()) >=3
+ORDER BY salary_gap DESC)A
+
